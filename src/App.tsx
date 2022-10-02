be9e55ca8a2d787a098669766422b583d1f65906
emanusantos <emanuel.lmds1@gmail.com>
2022-10-02 17:41:29 -0300

    feat: apuracao 2k22

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [results, setResults] = React.useState<any>();
+
+  React.useEffect(() => {
+    const fetchResults = async () => {
+      const fetchedResults = await fetch(
+        "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json"
+      );
+
+      const resultsInJSON = await fetchedResults.json();
+
+      setResults(resultsInJSON);
+    };
+
+    fetchResults();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Apuração 2022 - Presidente</h1>
+
+        <br />
+
+        {results?.cand?.map((cand: any) => (
+          <div key={cand?.sqcand}>
+            <h3>
+              {cand?.nm} - {cand?.pvap}%
+            </h3>
+          </div>
+        ))}
+
+        <br />
+        <br />
+        <br />
+        <br />
+        <h2>Votos apurados: {results?.vv}</h2>
+      </div>
     </div>
   );
 }
