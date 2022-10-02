7c0c37f3c1378c875d41f8054098b5b9cf830d47
emanusantos <emanuel.lmds1@gmail.com>
2022-10-02 18:33:54 -0300

    feat: loading state

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const [loading, setLoading] = React.useState(false);
   const [results, setResults] = React.useState<any>();
 
   React.useEffect(() => {
     const fetchResults = async () => {
+      setLoading(true);
+
       const fetchedResults = await fetch(
         "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json"
       );
@@ -13,6 +16,7 @@ function App() {
       const resultsInJSON = await fetchedResults.json();
 
       setResults(resultsInJSON);
+      setLoading(false);
     };
 
     fetchResults();
@@ -20,37 +24,41 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <h2>Apuração 2022 - Presidente</h2>
+      {loading && <h2>Carregando...</h2>}
 
-        <br />
+      {!loading && (
+        <div>
+          <h2>Apuração 2022 - Presidente</h2>
 
-        {results?.cand?.map((cand: any) => (
-          <div key={cand?.sqcand}>
-            <h4>
-              {cand?.nm} - {cand?.pvap}% ({cand?.vap} votos)
-            </h4>
-          </div>
-        ))}
+          <br />
 
-        <br />
-        <br />
-        <br />
-        <br />
-        <h3>
-          Votos apurados: {results?.vv} ({results?.pst}%)
-        </h3>
-        <button
-          onClick={() => window.location.reload()}
-          style={{
-            backgroundColor: "white",
-            border: "none",
-            cursor: "pointer",
-          }}
-        >
-          <h4 style={{ textDecoration: "underline" }}>Atualizar</h4>
-        </button>
-      </div>
+          {results?.cand?.map((cand: any) => (
+            <div key={cand?.sqcand}>
+              <h4>
+                {cand?.nm} - {cand?.pvap}% ({cand?.vap} votos)
+              </h4>
+            </div>
+          ))}
+
+          <br />
+          <br />
+          <br />
+          <br />
+          <h3>
+            Votos apurados: {results?.vv} ({results?.pst}%)
+          </h3>
+          <button
+            onClick={() => window.location.reload()}
+            style={{
+              backgroundColor: "white",
+              border: "none",
+              cursor: "pointer",
+            }}
+          >
+            <h4 style={{ textDecoration: "underline" }}>Atualizar</h4>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
